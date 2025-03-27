4348fad4b0090da1673416566b3db7fcfc139c77
Ben Frankel <ben.frankel7@gmail.com>
2025-03-27 03:40:38 -0700

    Update to Bevy 0.16.0 release candidate (#339)
    Fixes https://github.com/TheBevyFlock/bevy_new_2d/issues/334.

--- src/audio.rs
+++ src/audio.rs
@@ -1,35 +1,35 @@
 use bevy::prelude::*;
 
-/// An organizational marker component that should be added to a spawned [`AudioBundle`] if it is in the
+/// An organizational marker component that should be added to a spawned [`AudioPlayer`] if it is in the
 /// general "music" category (ex: global background music, soundtrack, etc).
 ///
 /// This can then be used to query for and operate on sounds in that category. For example:
 ///
 /// ```
-/// use bevy::prelude::*;
+/// use bevy::{audio::Volume, prelude::*};
 /// use bevy_new_2d::audio::Music;
 ///
-/// fn set_music_volume(sink_query: Query<&AudioSink, With<Music>>) {
-///     for sink in &sink_query {
-///         sink.set_volume(0.5);
+/// fn set_music_volume(mut sink_query: Query<&mut AudioSink, With<Music>>) {
+///     for mut sink in &mut sink_query {
+///         sink.set_volume(Volume::Linear(0.5));
 ///     }
 /// }
 /// ```
 #[derive(Component, Default)]
 pub struct Music;
 
-/// An organizational marker component that should be added to a spawned [`AudioBundle`] if it is in the
+/// An organizational marker component that should be added to a spawned [`AudioPlayer`] if it is in the
 /// general "sound effect" category (ex: footsteps, the sound of a magic spell, a door opening).
 ///
 /// This can then be used to query for and operate on sounds in that category. For example:
 ///
 /// ```
-/// use bevy::prelude::*;
+/// use bevy::{audio::Volume, prelude::*};
 /// use bevy_new_2d::audio::SoundEffect;
 ///
-/// fn set_sound_effect_volume(sink_query: Query<&AudioSink, With<SoundEffect>>) {
-///     for sink in &sink_query {
-///         sink.set_volume(0.5);
+/// fn set_sound_effect_volume(mut sink_query: Query<&mut AudioSink, With<SoundEffect>>) {
+///     for mut sink in &mut sink_query {
+///         sink.set_volume(Volume::Linear(0.5));
 ///     }
 /// }
 /// ```
